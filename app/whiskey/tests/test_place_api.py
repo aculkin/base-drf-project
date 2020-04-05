97fc711d1a02403fca1f38c584211fe8f2985fb6
amarbhatia95 <amarbhatia95@gmail.com>
2020-04-05 14:59:41 -0700

    Added create places feature

--- app/whiskey/tests/test_place_api.py
+++ app/whiskey/tests/test_place_api.py
@@ -61,3 +61,22 @@ class PrivatePlaceAPITest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], place.name)
+
+    def test_create_place_successful(self):
+        """Test creating place"""
+        payload = {'name': 'Campbell Brewery'}
+        self.client.post(PLACE_URL, payload)
+
+        exists = Place.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_place_invalid(self):
+        """Test creating invalid place fails"""
+        payload = {'name': ''}
+        res = self.client.post(PLACE_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
