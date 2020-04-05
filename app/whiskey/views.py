06225d91842f1ad56db46a0725e69ecf860bb7a6
amarbhatia95 <amarbhatia95@gmail.com>
2020-04-05 15:25:03 -0700

    Re-factored tags and places

--- app/whiskey/views.py
+++ app/whiskey/views.py
@@ -7,37 +7,29 @@ from core.models import Tag, Place
 from whiskey import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class BaseWhiskeyAttrViewset(viewsets.GenericViewSet,
+                             mixins.ListModelMixin,
+                             mixins.CreateModelMixin):
+    """Base viewset for user owned whiskey attributes"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
-        """Create a new tag"""
-        serializer.save(user=self.request.user)
+        """Create a new object"""
+        return serializer.save(user=self.request.user)
 
 
-class PlaceViewSet(viewsets.GenericViewSet,
-                   mixins.ListModelMixin,
-                   mixins.CreateModelMixin):
+class TagViewSet(BaseWhiskeyAttrViewset):
+    """Manage tags in the database"""
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class PlaceViewSet(BaseWhiskeyAttrViewset):
     """Manage places in the database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Place.objects.all()
     serializer_class = serializers.PlaceSerializer
-
-    def get_queryset(self):
-        """Return objects for the current authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a new Place"""
-        return serializer.save(user=self.request.user)
